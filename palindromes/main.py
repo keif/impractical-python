dda34b22bfe20d134f382c0bce5a3d5e4cd74fbe
Keith Baker <github@baker.is>
2025-11-29 14:44:08 -0500

    palindrome pseudo code

--- palindromes/main.py
+++ palindromes/main.py
@@ -3,6 +3,21 @@
 
 def main():
     """Finding Palindromes."""
+    # pseudocode
+    # How do we find palindromes?
+    # Compare a word to itself sliced backward
+    # i.e. word = nurses
+    # word[:] // nurses
+    # word[::-1] // sesrun
+
+    # load our dictionary word list
+    # create an empty list to hold palindromes
+    # loop through each word in the word list:
+    #   If the word sliced forward is the same backward:
+    #       Append word to palindrome list
+    #
+
+    # code stub
     # try:
     #     with open("words.txt") as in_file:
     # do something
