ac994aa206f675b3925185c39da077b864da9f65
Keith Baker <github@baker.is>
2025-11-29 18:37:31 -0500

    palingram pseudocode

---
+++ palingrams/main.py
@@ -0,0 +1,31 @@
+"""Palingrams.
+
+Find two-word palingrams.
+"""
+
+
+def main():
+    """Palingrams main function."""
+    # pseudocode
+    # EX. NURSES RUN
+    # EX. STIR GRITS
+    # Load dictionary of words
+    # Empty list to hold palingrams
+    # For word in word list:
+    #   Get length of word
+    #   if length > 2:
+    #       loop through letters in the word:
+    #           if reversed word fragment at the front of word is in
+    #               word list and letters after form a palindomic sequence:
+    #               append word and reversed word to palingram list
+    #           if reversed word fragment at end of word is in word
+    #           list and letter before form a palindromic sequence:
+    #               Append reversed word and word to palingram list
+    # Sort palingram list alphabetically
+    # Print word-pair palingrams from palingram list
+
+    pass
+
+
+if __name__ == "__main__":
+    main()
