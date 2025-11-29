b7a96bfd5d5ddfa2eff8666e05e81d117569261c
Keith Baker <github@baker.is>
2025-11-29 14:38:53 -0500

    init: palindrome dictionary

---
+++ palindromes/main.py
@@ -0,0 +1,16 @@
+"""Finding Palindromes."""
+
+
+def main():
+    """Finding Palindromes."""
+    # try:
+    #     with open("words.txt") as in_file:
+    # do something
+    # pass
+    # except OSError as e:
+    #     print(f"{e}\nError opening {file}. Terminating program.", file=sys.stderr)
+    #     sys.exit(1)
+
+
+if __name__ == "__main__":
+    main()
